b3b22b0fe9580d7bf6ee9b50994ffb55a3c90671
yati-10 <yati.manjunath18@gmail.com>
2025-09-13 16:22:33 -0400

    init

---
+++ README.md
@@ -0,0 +1,32 @@
+[![Athena Award Badge](https://img.shields.io/endpoint?url=https%3A%2F%2Faward.athena.hackclub.com%2Fapi%2Fbadge)](https://award.athena.hackclub.com?utm_source=readme)
+# 🧺 Falling Object Catcher Game
+
+This project is a fun and simple p5.js + p5.play arcade style game where the user has to catch falling objects using a basket. And earn points by catching them and not letting too many fall, or else they lose!
+
+---
+
+## 🎮 How to Play
+
+* You should use the left ⬅️ and right ➡️ arrow keys to move the basket.
+* Catch the falling objects to gain points.
+* Each catch will give you +1 point and missing one will subtract -1 point.
+* You have to reach 15 points to win or fall to -5 points to lose.
+
+---
+
+## 🌟 Features
+
+There is a custom background and also a basket as a catcher. Random colored falling circles that have different speeds and a collision detector which supports the score tracking to increase and decrease points.
+
+---
+
+## 🛠️ Built With
+
+* [p5.js](https://p5js.org/)
+* [p5.play](https://molleindustria.github.io/p5.play/)
+
+---
+
+## 🧠 What I Learned
+
+How to move an object using arrow keys, how to increase or decrease points by detecting sprite collision. Win/loose statements and also how to add images to objects and backgrounds.
---
+++ script.js
@@ -0,0 +1,107 @@
+/* VARIABLES */
+let catcher;
+let fallingObject;
+let fallingObject2;
+let score = 0;
+
+/* PRELOAD LOADS FILES */
+function preload(){
+  backGround = loadImage("assets/bg.png");
+  basketImg = loadImage("assets/basket.png");
+  basketImg.resize(60, 0);
+}
+
+/* SETUP RUNS ONCE */
+function setup() {
+  createCanvas(400,400);
+
+  // Falling objects
+  fallingObject = new Sprite(random(10,390), -10,20);
+  fallingObject2 = new Sprite(random(10,390),-10,20);
+  fallingObject.vel.y = random(2,4);
+  fallingObject2.vel.y = random(2,4);
+  fallingObject.color = color(0, 255, 0); 
+  fallingObject2.color = color(0, 0, 255); 
+
+  // Create catcher 
+  catcher = new Sprite(200,380,100,20);
+  catcher.color = color(95,158,160);
+  catcher.collider = "k";
+  catcher.image = basketImg;
+
+  catcher.width = 10; 
+  catcher.height = 10;
+} 
+
+/* DRAW LOOP REPEATS */
+function draw() {
+  image(backGround,0,0,400,400);
+
+  // Catcher movement
+  if (kb.pressing("left")){
+    catcher.vel.x = -5;
+  }
+  else if (kb.pressing("right")){
+    catcher.vel.x = 5;
+  } else {
+    catcher.vel.x = 0;
+    catcher.vel.y = 0; 
+  }
+
+  // Catcher on screen
+  if (catcher.y > 380) catcher.y = 380;
+  if (catcher.x > width) catcher.x = width;
+
+  // Score
+  fill(0);
+  textSize(12);
+  text("Score: " + score, 10, 20);
+
+  // Object collisions
+  if (fallingObject.collide(catcher)){
+    score += 1;
+    resetObject(fallingObject);
+    fallingObject.color = color(random(255), random(255), random(255));
+  } else if (fallingObject.y >= height){
+    score -= 1;
+    resetObject(fallingObject);
+    fallingObject.color = color(random(255), random(255), random(255));
+  }
+
+  if (fallingObject2.collide(catcher)){
+    score += 1;
+    resetObject(fallingObject2);
+    fallingObject2.color = color(random(255), random(255), random(255));
+  } else if (fallingObject2.y >= height){
+    score -= 1;
+    resetObject(fallingObject2);
+    fallingObject2.color = color(random(255), random(255), random(255));
+  }
+
+  // Win/lose 
+  if (score >= 15){
+    gameOver("You\n win!", 30, 110);
+  } else if (score <= -5){
+    gameOver("You lose!", 70, 80);
+  }  
+}
+
+function resetObject(obj) {
+  obj.pos = {x: random(width), y: -10};
+  obj.vel.y = random(2,4);
+  obj.direction = "down";
+}
+
+function gameOver(message, x, y) {
+  background('grey');
+  fill('black');
+  textSize(35);
+  text(message, x, y);
+
+  // Stop  objects
+  fallingObject.pos = {x: -200, y: -200};
+  fallingObject2.pos = {x: -200, y: -200};
+  catcher.pos = {x: -200, y: -200};
+  fallingObject.vel.y = 0;
+  fallingObject2.vel.y = 0;
+}
